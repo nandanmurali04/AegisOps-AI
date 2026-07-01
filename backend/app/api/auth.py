1cbecd100a5669e024f219a4c569c4d3ff0b5a4e
nandanmurali04 <nandan04in@gmail.com>
2026-07-01 10:46:45 +0530

    Completed Phase 3 - JWT Authentication and RBAC

--- backend/app/api/auth.py
+++ backend/app/api/auth.py
@@ -3,12 +3,7 @@ from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from sqlalchemy.orm import Session
 
 from app.db.database import SessionLocal
-from app.schemas.user import (
-    UserCreate,
-    UserResponse,
-    LoginRequest,
-    Token
-)
+from app.schemas.user import UserCreate, UserResponse, LoginRequest, Token
 from app.services.auth import create_user, login_user
 from app.utils.security import verify_access_token
 
@@ -25,17 +20,34 @@ def get_db():
         db.close()
 
 
-# -----------------------
+# ------------------------------------
+# Get Current User from JWT
+# ------------------------------------
+def get_current_user(
+    credentials: HTTPAuthorizationCredentials = Depends(security)
+):
+    payload = verify_access_token(credentials.credentials)
+
+    if payload is None:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid or expired token"
+        )
+
+    return payload
+
+
+# ------------------------------------
 # Register
-# -----------------------
+# ------------------------------------
 @router.post("/register", response_model=UserResponse)
 def register(user: UserCreate, db: Session = Depends(get_db)):
     return create_user(db, user)
 
 
-# -----------------------
+# ------------------------------------
 # Login
-# -----------------------
+# ------------------------------------
 @router.post("/login", response_model=Token)
 def login(user: LoginRequest, db: Session = Depends(get_db)):
     token = login_user(db, user.email, user.password)
@@ -49,22 +61,33 @@ def login(user: LoginRequest, db: Session = Depends(get_db)):
     return token
 
 
-# -----------------------
-# Protected Route
-# -----------------------
+# ------------------------------------
+# User Profile (Protected)
+# ------------------------------------
 @router.get("/profile")
 def profile(
-    credentials: HTTPAuthorizationCredentials = Depends(security)
+    current_user=Depends(get_current_user)
 ):
-    payload = verify_access_token(credentials.credentials)
+    return {
+        "message": "Welcome!",
+        "user": current_user
+    }
 
-    if payload is None:
+
+# ------------------------------------
+# Admin Dashboard (Admin Only)
+# ------------------------------------
+@router.get("/admin/dashboard")
+def admin_dashboard(
+    current_user=Depends(get_current_user)
+):
+    if current_user.get("role") != "admin":
         raise HTTPException(
-            status_code=401,
-            detail="Invalid or expired token"
+            status_code=403,
+            detail="Access denied. Admins only."
         )
 
     return {
-        "message": "Welcome!",
-        "user": payload
+        "message": "Welcome Admin!",
+        "user": current_user
     }
